814877e430532cf2df14901e65b67e55f05d567f
Soren-Madsen <sorenmadsen13@gmail.com>
2026-02-11 15:14:01 +0100

    #21 Refactor FilmFakerSeeder to assign default genres to film seeder

--- database/seeders/FilmFakerSeeder.php
+++ database/seeders/FilmFakerSeeder.php
@@ -8,25 +8,26 @@ use Faker\Factory as Faker;
 use Illuminate\Support\Facades\DB;
 
 class FilmFakerSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
     {
-        $faker = Faker::create();
+        /**
+         * Run the database seeds.
+         */
+        public function run(): void
+        {
+            $faker = Faker::create();
+            $genres = ['thriller', 'action', 'drama', 'love'];
 
-        for ($i = 0; $i < 10; $i++) {
-            DB::table('films')->insert([
-                'name' => $faker->words(3, true),
-                'year' => $faker->year(),
-                'genre' => $faker->word(),
-                'country' => $faker->country(),
-                'duration' => $faker->numberBetween(60, 180),
-                'img_url' => $faker->imageUrl(400, 600, 'movies'),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
+            for ($i = 0; $i < 10; $i++) {
+                DB::table('films')->insert([
+                    'name' => $faker->words(3, true),
+                    'year' => $faker->year(),
+                    'genre' => $faker->randomElement($genres),
+                    'country' => $faker->country(),
+                    'duration' => $faker->numberBetween(60, 180),
+                    'img_url' => $faker->imageUrl(400, 600, 'movies'),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
         }
     }
-}
